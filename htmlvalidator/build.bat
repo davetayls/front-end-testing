b45ccb40a901fcab8178585cf480004fab1ecd67
Dave Taylor <dave@the-taylors.org>
2011-08-04 16:57:36 +0100

    updated build

--- htmlvalidator/build.bat
+++ htmlvalidator/build.bat
@@ -1,3 +1,4 @@
+set VERNUM=1b2
 cd ..
 rmdir htmlvalidator-dist /s /q
 mkdir htmlvalidator-dist
@@ -8,6 +9,14 @@ copy htmlvalidator\*.json htmlvalidator-dist\
 copy htmlvalidator\run*.bat htmlvalidator-dist\
 
 copy htmlvalidator\js.jar htmlvalidator-dist\
-call lib\Rivet\Rivet.Console.exe htmlvalidator-dist\ -v:VERSION_NUMBER=1b1 -v:RELEASED="%DATE%-%TIME%"
+call lib\Rivet\Rivet.Console.exe htmlvalidator-dist\ -v:VERSION_NUMBER=%VERNUM% -v:RELEASED="%DATE%-%TIME%"
+
+git checkout dist
+rmdir htmlvalidator
+rename htmlvalidator-dist htmlvalidator
+git add .
+git commit -am "release %VERNUM%"
+git push
+git checkout master
 
 pause
