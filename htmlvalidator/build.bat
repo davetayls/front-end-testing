a110e40214b506f76ce485f9769d29c7717e2f32
Dave Taylor <dave@the-taylors.org>
2011-08-05 10:30:43 +0100

    made build a little smoother

--- htmlvalidator/build.bat
+++ htmlvalidator/build.bat
@@ -1,6 +1,6 @@
-set VERNUM=1b2
+set VERNUM=1b3
 cd ..\..
-git clone -b dist front-end-testing\.git front-end-testing.dist\
+call git clone -b dist front-end-testing\.git front-end-testing.dist\
 
 rmdir front-end-testing.dist\htmlvalidator /s /q
 mkdir front-end-testing.dist\htmlvalidator
@@ -11,11 +11,17 @@ copy front-end-testing\htmlvalidator\*.json front-end-testing.dist\htmlvalidator
 copy front-end-testing\htmlvalidator\run*.bat front-end-testing.dist\htmlvalidator\
 
 copy front-end-testing\htmlvalidator\js.jar front-end-testing.dist\htmlvalidator\
-call lib\Rivet\Rivet.Console.exe front-end-testing.dist\htmlvalidator\ -v:VERSION_NUMBER=%VERNUM% -v:RELEASED="%DATE%-%TIME%"
+call front-end-testing\lib\Rivet\Rivet.Console.exe front-end-testing.dist\htmlvalidator\ -v:VERSION_NUMBER=%VERNUM% -v:RELEASED="%DATE%-%TIME%"
 
 cd front-end-testing.dist
-git add .
-git commit -am "release %VERNUM%"
-git push
+call git add .
+call git tag %VERNUM%
+call git commit -am "release %VERNUM%"
+call git push --tags
+
+cd ..
+rmdir front-end-testing.dist /s /q
+
+cd front-end-testing
 
 pause
